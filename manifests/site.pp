c1fa1e04e25a8987b6413210622d8e502b2afdc2
pjfoley <peter@ifoley.id.au>
2015-01-14 22:16:16 +1100

    Test installing dotfiles

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,16 @@
 node 'default' {
-  fail ('Still need to set something up')
+  #fail ('Still need to set something up')
+  
+  vcsrepo { '/root/.dotfiles':
+    ensure => present,
+    provider => git,
+    source => 'https://github.com/pjfoley/dotfiles-home.git',
+  }
+  
+  dotfiles { '/root':
+    dotfiles => '/root/.dotfiles',
+    owner => 'root',
+    group => 'root',
+    require => Vcsrepo[/root/.dotfiles],
+  }
 }
